1fbb9ff42bf835009daeb5c1ea4c07376166537c
sotoro <65254938+sotoro@users.noreply.github.com>
2022-02-16 11:19:05 -0800

    Update js to new domain

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 $(function(){
 	var includes = $('[data-include]');
 	jQuery.each(includes, function(){
-		var file = 'https://LaurelHillsSL.github.io/Academy/' + $(this).data('include') + '.html';
+		var file = 'https://laurelhillsacademy-sl.com/' + $(this).data('include') + '.html';
 		$(this).load(file);
 	});
 });
